9618d8ea0ea5e68b26e050162ef7416026bb1915
Vanessa Gudan <vanegudan@gmail.com>
2026-08-07 10:30:48 +0000

    Fri, Aug 7, 2026, 1:30 PM +03:00

---
+++ views/3.sql
@@ -0,0 +1,4 @@
+ count how many prints by Hokusai include “Fuji” in the English title.
+ Though all of Hokusai’s prints focused on Mt. Fuji,
+ in how many did “Fuji” make it into the title?
+SELECT COUNT()
