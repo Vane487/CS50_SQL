2933bbd7e30da0671e94e4d4ad0d9e050c71fc13
Vanessa Gudan <vanegudan@gmail.com>
2026-08-08 07:11:13 +0000

    Sat, Aug 8, 2026, 10:11 AM +03:00

---
+++ views/7.sql
@@ -0,0 +1,7 @@
+SELECT english_title
+FROM views
+WHERE artist = 'Hiroshige'
+ORDER BY brightness DESC
+LIMIT 5;
+
+
---
+++ views/8.sql
@@ -0,0 +1,2 @@
+In 8.sql, write a SQL query to list the English titles of the 5 prints with the least contrast by Hokusai,
+ from least to highest contrast.
