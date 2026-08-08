17d0ee062f856cd0aa354e903dd31a5971175493
Vanessa Gudan <vanegudan@gmail.com>
2026-08-08 07:17:42 +0000

    Sat, Aug 8, 2026, 10:17 AM +03:00

--- views/8.sql
+++ views/8.sql
@@ -1,2 +1,6 @@
-In 8.sql, write a SQL query to list the English titles of the 5 prints with the least contrast by Hokusai,
- from least to highest contrast.
+
+SELECT english_title
+FROM views
+WHERE artist = 'Hokusai'
+ORDER BY contrast ASC
+LIMIT 5;
