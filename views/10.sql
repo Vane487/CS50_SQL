32ae6212145523c4bb0da4f89467dfae352257e1
Vanessa Gudan <vanegudan@gmail.com>
2026-08-08 07:27:57 +0000

    Sat, Aug 8, 2026, 10:27 AM +03:00

--- views/10.sql
+++ views/10.sql
@@ -1,6 +1,7 @@
-In 10.sql, write a SQL query to answer a question of your choice about the prints. The query should:
-Make use of AS to rename a column
-Involve at least one condition, using WHERE
-Sort by at least one column, using ORDER BY
 
-SELECT 
+
+SELECT english_title AS 'Engish names of highest brightnesses of Hiroshige '
+FROM views
+WHERE artist = 'Hiroshige'
+ORDER BY brightness DESC
+LIMIT 2;
