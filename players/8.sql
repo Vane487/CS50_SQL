87bd0981d968af4ab76f655a3770a999b578b892
Vanessa Gudan <vanegudan@gmail.com>
2026-08-18 11:42:07 +0000

    Tue, Aug 18, 2026, 2:42 PM +03:00

--- players/8.sql
+++ players/8.sql
@@ -3,3 +3,4 @@ a SQL query to find the average height and weight,
  rounded to two decimal places,
  of baseball players who debuted on or after January 1st, 2000.
   Return the columns with the name “Average Height” and “Average Weight”, respectively.
+SELECT 
