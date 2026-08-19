6efc44a3d764757208029827b04629a47c86a992
Vanessa Gudan <vanegudan@gmail.com>
2026-08-19 16:42:38 +0000

    Wed, Aug 19, 2026, 7:42 PM +03:00

--- packages/log.sql
+++ packages/log.sql
@@ -47,7 +47,12 @@ WHO HAS THE GIFT
 SELECT name FROM drivers
 WHERE id = ( SELECT driver_id
        FROM scans
-       WHERE package_id = SELECT 
+       WHERE package_id = (SELECT id
+             FROM packages
+             WHERE from_address_id = (SELECT id
+                   FROM addresses
+                   WHERE address = '109 Tileston Street' )
 
-       )
+
+       ))
 
