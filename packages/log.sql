9d462d3507b3596e2a08dcc1f79ba6c1f63459b6
Vanessa Gudan <vanegudan@gmail.com>
2026-08-19 10:57:39 +0000

    Wed, Aug 19, 2026, 1:57 PM +03:00

--- packages/log.sql
+++ packages/log.sql
@@ -22,7 +22,7 @@ SELECT contents FROM packages WHERE from_address_id IS NULL;
 -- Finding out the address it ended up at  --
 
 At what type of address did the Devious Delivery end up?:
-
+SELECT 
 
 -- *** The Forgotten Gift ***
 
