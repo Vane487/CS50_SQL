68776aa59b469491e2fddfb5736c30ab6f471107
Vanessa Gudan <vanegudan@gmail.com>
2026-08-19 16:47:19 +0000

    Wed, Aug 19, 2026, 7:47 PM +03:00

--- packages/log.sql
+++ packages/log.sql
@@ -42,19 +42,19 @@ AND to_address_id = (
 );
 
 
-WHO HAS THE GIFT
+-- WHO HAS THE GIFT -- 
 
 SELECT name FROM drivers
-WHERE id = ( SELECT driver_id
-       FROM scans
-       WHERE package_id = (SELECT id
-             FROM packages
-             WHERE from_address_id = (SELECT id
-                   FROM addresses
-                   WHERE address = '109 Tileston Street' )
-       AND to_address_id = (
-                     SELECT id FROM addresses WHERE
-                     address = '728 Maple Place'
-                     
-)))
+WHERE id = (
+    SELECT driver_id
+    FROM scans
+    WHERE package_id = (
+        SELECT id FROM packages
+        WHERE from_address_id = (SELECT id FROM addresses WHERE address = '109 Tileston Street')
+          AND to_address_id = (SELECT id FROM addresses WHERE address = '728 Maple Place')
+    )
+    ORDER BY timestamp DESC
+    LIMIT 1
+);
+
 
