570331f0827d43b38a119791a0434c527a801c74
Vanessa Gudan <vanegudan@gmail.com>
2026-08-19 10:26:47 +0000

    Wed, Aug 19, 2026, 1:26 PM +03:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,7 +1,7 @@
 
 -- *** The Lost Letter ***  live over at 900 Somerville Avenue
 SELECT contents FROM packages
-WHERE 
+WHERE from_address_id = (SELECT id FROM )
 
 -- *** The Devious Delivery ***
 
