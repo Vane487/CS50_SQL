b72afceefba7e98dec8b944fdbff8b3d6dceb684
Vanessa Gudan <vanegudan@gmail.com>
2026-08-19 10:47:26 +0000

    Wed, Aug 19, 2026, 1:47 PM +03:00

--- packages/log.sql
+++ packages/log.sql
@@ -4,14 +4,15 @@ At what type of address did the Lost Letter end up?:
 At what address did the Lost Letter end up?:
 
 SELECT address, type FROM addresses
-WHERE id =
-        ( SELECT address_id FROM scans
-        WHERE action = 'Drop' AND package_id = (
-            SELECT id FROM packages
-            WHERE from_address_id = ( SELECT id from addresses WHERE address = ' 
-
-            )
+WHERE id = (
+    SELECT address_id FROM scans
+    WHERE action = 'Drop' AND package_id = (
+        SELECT id FROM packages
+        WHERE from_address_id = (
+            SELECT id FROM addresses WHERE address = '900 Somerville Avenue'
         )
+    )
+);
 
 
 -- *** The Devious Delivery ***
