8efc5dd55fa44ec09ca00e4b2d7e339704d5e806
Vanessa Gudan <vanegudan@gmail.com>
2026-08-24 17:26:14 +0000

    Complete Week 4 BnB

---
+++ Week 4/bnb/no_descriptions.sql
@@ -0,0 +1,3 @@
+CREATE VIEW "no_descriptions" AS
+SELECT "id", "name", "host_name", "neighbourhood", "room_type", "price", "minimum_nights", "maximum_nights", "accommodates", "bedrooms"
+FROM "listings";
